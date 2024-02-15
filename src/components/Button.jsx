d18c52402f7200be0f31be6f356c0709726a3c65
Guillaume Louis <guillaume.louis@uni.lu>
2024-02-15 09:00:10 +0100

    add props to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ export const Button = (props) => {
   const [state, setState] = useState(0)
   return (
     <div>
-      <button id='click-btn' className='shared-btn' onClick={() => setState((s) => s + 1)}>Title is: {props.title}</button>
+      <button id='click-btn' className='shared-btn' onClick={props.onClick}>Title is: {props.title}</button>
     </div>
   )
 }
